581877c388bd385da90365e5ee63b2844f64c3f8
Toxe <toxe@toxe.net>
2019-06-28 17:10:24 +0200

    Project Euler Problem 8

--- project_euler.cpp
+++ project_euler.cpp
@@ -96,6 +96,16 @@ int nth_prime(int n)
     return number;
 }
 
+long long calc_adjacent_digits_product(std::string::const_iterator begin, std::string::const_iterator end)
+{
+    long long prod = 1;
+
+    for (auto p = begin; p < end; ++p)
+        prod *= *p - 48;
+
+    return prod;
+}
+
 int euler001()
 {
     int sum = 0;
@@ -164,6 +174,25 @@ int euler006()
     }
 
     return sum * sum - sum_of_squares;
+}
+
+long long euler008(const std::string& big_number)
+{
+    std::string::const_iterator begin = big_number.begin();
+    std::string::const_iterator end = begin + 13;
+    long long max = 0;
+
+    while (end <= big_number.end()) {
+        auto prod = calc_adjacent_digits_product(begin, end);
+
+        if (prod > max)
+            max = prod;
+
+        ++begin;
+        ++end;
+    }
+
+    return max;
 }
 
 static void BM_Euler001(benchmark::State& state)
@@ -238,6 +267,34 @@ static void BM_Euler007_NthPrime(benchmark::State& state)
         benchmark::DoNotOptimize(nth_prime(state.range(0)));
 }
 
+static void BM_Euler008(benchmark::State& state)
+{
+    const std::string big_number{
+        "73167176531330624919225119674426574742355349194934"
+        "96983520312774506326239578318016984801869478851843"
+        "85861560789112949495459501737958331952853208805511"
+        "12540698747158523863050715693290963295227443043557"
+        "66896648950445244523161731856403098711121722383113"
+        "62229893423380308135336276614282806444486645238749"
+        "30358907296290491560440772390713810515859307960866"
+        "70172427121883998797908792274921901699720888093776"
+        "65727333001053367881220235421809751254540594752243"
+        "52584907711670556013604839586446706324415722155397"
+        "53697817977846174064955149290862569321978468622482"
+        "83972241375657056057490261407972968652414535100474"
+        "82166370484403199890008895243450658541227588666881"
+        "16427171479924442928230863465674813919123162824586"
+        "17866458359124566529476545682848912883142607690042"
+        "24219022671055626321111109370544217506941658960408"
+        "07198403850962455444362981230987879927244284909188"
+        "84580156166097919133875499200524063689912560717606"
+        "05886116467109405077541002256983155200055935729725"
+        "71636269561882670428252483600823257530420752963450"};
+
+    for (auto _ : state)
+        benchmark::DoNotOptimize(euler008(big_number));
+}
+
 BENCHMARK(BM_Euler001);
 BENCHMARK(BM_Euler002);
 
@@ -299,5 +356,7 @@ BENCHMARK(BM_Euler007_IsPrime_LongLong)->Arg(999999000003);
 BENCHMARK(BM_Euler007_NthPrime)->Arg(6);
 BENCHMARK(BM_Euler007_NthPrime)->Arg(100);
 BENCHMARK(BM_Euler007_NthPrime)->Arg(10001);
+
+BENCHMARK(BM_Euler008);
 
 BENCHMARK_MAIN();
