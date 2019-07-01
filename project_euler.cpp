5b51e735ba51274a8cbffa6d8b400e3c7dc985af
Toxe <toxe@toxe.net>
2019-07-01 11:48:43 +0200

    Project Euler Problem 15

--- project_euler.cpp
+++ project_euler.cpp
@@ -464,6 +464,28 @@ int euler014()
     return longest_sequence_number;
 }
 
+long long euler015(int grid_size)
+{
+    // create grid with one additional column and row
+    const int grid_size_plus_one = grid_size + 1;
+    std::unique_ptr<long long[]> grid{new long long[(std::size_t) (grid_size_plus_one * grid_size_plus_one)]};
+
+    // fill additional column and row with 1
+    for (int i = 0; i < grid_size_plus_one; ++i) {
+        grid[(std::size_t) (i * grid_size_plus_one + grid_size)] = 1;
+        grid[(std::size_t) (grid_size * grid_size_plus_one + i)] = 1;
+    }
+
+    for (int y = grid_size - 1; y >= 0; --y) {
+        for (int x = grid_size - 1; x >= 0; --x) {
+            grid[(std::size_t) (y * grid_size_plus_one + x)] = grid[(std::size_t) (y * grid_size_plus_one + x + 1)] +
+                                                               grid[(std::size_t) ((y + 1) * grid_size_plus_one + x)];
+        }
+    }
+
+    return grid[0];
+}
+
 static void BM_Euler001(benchmark::State& state)
 {
     for (auto _ : state)
@@ -606,6 +628,12 @@ static void BM_Euler014(benchmark::State& state)
         benchmark::DoNotOptimize(euler014());
 }
 
+static void BM_Euler015(benchmark::State& state)
+{
+    for (auto _ : state)
+        benchmark::DoNotOptimize(euler015(state.range(0)));
+}
+
 BENCHMARK(BM_Euler001);
 BENCHMARK(BM_Euler002);
 
@@ -686,5 +714,9 @@ BENCHMARK(BM_Euler012_IntegerFactorization)->Arg(76576500);
 
 BENCHMARK(BM_Euler013);
 BENCHMARK(BM_Euler014);
+
+BENCHMARK(BM_Euler015)->Arg(2);
+BENCHMARK(BM_Euler015)->Arg(10);
+BENCHMARK(BM_Euler015)->Arg(20);
 
 BENCHMARK_MAIN();
